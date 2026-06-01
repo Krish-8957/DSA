58b162d93775d46b47e6745782713c7b8bb634c0
Krish Verma <krishverma.2433303@gmail.com>
2026-06-01 14:50:06 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (24.39%) - LeetSync

---
+++ 1635-number-of-good-pairs/number-of-good-pairs.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+   unordered_map<int,int>freq;
+   int count=0;
+
+   for(int i=0;i<nums.size();i++){
+    count=count+freq[nums[i]];
+    freq[nums[i]]=freq[nums[i]]+1;
+   }
+   return count;
+        
+    }
+};
